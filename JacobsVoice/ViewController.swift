4c728f58aaa956de470393f5d8a7649eb2e58028
Klemen Kastelic <klemen.kastelic@xlab.si>
2015-05-17 00:05:31 +0200

    Initial Commit

---
+++ JacobsVoice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JacobsVoice
+//
+//  Created by Klemen Kastelic on 17/05/15.
+//  Copyright (c) 2015 Klemen Kastelic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
