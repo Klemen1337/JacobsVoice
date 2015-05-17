9224020e95b3000d537d68266ab4b9ffadb93b33
Andraz <andraz.pajtler@klika.si>
2015-05-17 06:44:55 +0200

    Incomplete butttons constants

--- JacobsVoice/Constants.swift
+++ JacobsVoice/Constants.swift
@@ -8,8 +8,49 @@
 
 import Foundation
 
-let EMOTIONS = [
+let EMOTIONS_MAIN = [
     "HAPPY", "SAD", "ANGRY", "HUNGRY",
     "TIRED", "EXCITED", "BORED", "GOOD",
     "SCARED", "SLEEPY", "HUNGRY", "MAD"
+]
+
+let EMOTIONS_JOINTS = [
+	"I am", "You are", "He is", "She is"
+]
+
+let FOOD_MAIN = [
+	"Breakfast", "Launch", "Dinner", "Snack",
+	"Pizza", "Pancake", "I'm full", "delicious",
+	"disgusting", "More please", "Meat", "Vegetables"
+]
+let FOOD_MAIN_JOINTS = [
+	"That's", "I don't eat", "I love"
+	]
+
+let FRIENDS_MAIN = [
+	"Klemen", "Andraz", "Igor", "Danijel",
+	"TIRED", "EXCITED", "BORED", "GOOD",
+	"SCARED", "SLEEPY", "HUNGRY", "MAD"
+]
+let FRIENDS_JOINTS = [
+	
+	]
+
+let PLACES_MAIN = [
+	"Home", "House", "Garage", "School", "Bank",
+	"Street", "Hospital", "Library", "Restaurant",
+	"Church"
+]
+let PLACES_JOINTS = [
+	"I'm going", "I'm at", "It's", "He's",
+	"She's",
+]
+
+let SCHOOL_MAIN = [
+	"", "", "", "",
+	"", "", "", "",
+	"", "", "", ""
+]
+let SCHOOL_JOINTS = [
+	
 ]
