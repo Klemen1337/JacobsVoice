d72e6ff284709df9edf20bba87519e6c964f1fa9
Klemen Kastelic <klemen.kastelic@xlab.si>
2015-05-17 07:13:39 +0200

    Constants updates, Added button functions

--- JacobsVoice/Constants.swift
+++ JacobsVoice/Constants.swift
@@ -8,49 +8,64 @@
 
 import Foundation
 
+let CATEGORIES = [
+    "Emotions",
+    "Food",
+    "Friends",
+    "Places",
+    "School"
+]
+
 let EMOTIONS_MAIN = [
-    "HAPPY", "SAD", "ANGRY", "HUNGRY",
-    "TIRED", "EXCITED", "BORED", "GOOD",
-    "SCARED", "SLEEPY", "HUNGRY", "MAD"
+    "Happy", "Sad", "Angry", "Hungry",
+    "Tired", "Excited", "Bored", "Good",
+    "Scared", "Sleepy", "Hungry", "Mad"
 ]
 
 let EMOTIONS_JOINTS = [
 	"I am", "You are", "He is", "She is"
 ]
 
+
 let FOOD_MAIN = [
 	"Breakfast", "Launch", "Dinner", "Snack",
-	"Pizza", "Pancake", "I'm full", "delicious",
-	"disgusting", "More please", "Meat", "Vegetables"
+	"Pizza", "Pancake", "I'm full", "Delicious",
+	"Disgusting", "More please", "Meat", "Vegetables"
 ]
+
 let FOOD_MAIN_JOINTS = [
 	"That's", "I don't eat", "I love"
-	]
+]
+
 
 let FRIENDS_MAIN = [
 	"Klemen", "Andraz", "Igor", "Danijel",
-	"TIRED", "EXCITED", "BORED", "GOOD",
-	"SCARED", "SLEEPY", "HUNGRY", "MAD"
+    "", "", "", "",
+    "", "", "", ""
 ]
+
 let FRIENDS_JOINTS = [
-	
-	]
+	"He is", "She is", "Talk to", "Call"
+]
+
 
 let PLACES_MAIN = [
-	"Home", "House", "Garage", "School", "Bank",
-	"Street", "Hospital", "Library", "Restaurant",
-	"Church"
-]
-let PLACES_JOINTS = [
-	"I'm going", "I'm at", "It's", "He's",
-	"She's",
+	"Home", "House", "Garage", "School",
+    "Bank","Street", "Hospital", "Library",
+    "Restaurant", "Church", "", ""
 ]
 
-let SCHOOL_MAIN = [
-	"", "", "", "",
-	"", "", "", "",
-	"", "", "", ""
+let PLACES_JOINTS = [
+	"I'm going", "I'm at", "He's", "She's",
 ]
+
+
+let SCHOOL_MAIN = [
+	"Bus", "Desk", "Pen", "Read",
+	"Draw", "Math", "", "",
+    "", "", "", "",
+]
+
 let SCHOOL_JOINTS = [
-	
+	"My", "His", "Hers", "Their"
 ]
--- JacobsVoice/ViewController.swift
+++ JacobsVoice/ViewController.swift
@@ -11,19 +11,25 @@ import AVFoundation
 
 class ViewController: UIViewController {
     
+    @IBOutlet var categoryButtons: [UIButton]!
+    @IBOutlet var bottomButtons: [UIButton]!
     @IBOutlet var buttons: [UIButton]!
     @IBOutlet weak var textField: UITextField!
     var speechHelper = SpeechHelper()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(EMOTIONS)
-        // Do any additional setup after loading the view, typically from a nib.
+        setCategoryButtonsTitles(CATEGORIES)
+        setEmotions(EMOTIONS_MAIN, bottomButtonsArray: EMOTIONS_JOINTS)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func ClearButtonClicked(sender: AnyObject) {
+        textField.text = ""
     }
     
     @IBAction func SpeakButtonClicked(sender: AnyObject) {
@@ -36,9 +42,55 @@ class ViewController: UIViewController {
         appendText(sender.titleLabel!!.text!)
     }
     
+    @IBAction func buttonCategoryClicked(sender: AnyObject) {
+        var button:UIButton = sender as! UIButton
+        switch(sender.titleLabel!!.text!){
+            case "Emotions":
+                setEmotions(EMOTIONS_MAIN, bottomButtonsArray: EMOTIONS_JOINTS)
+                break
+            case "Food":
+                setEmotions(FOOD_MAIN, bottomButtonsArray: FOOD_MAIN_JOINTS)
+                break
+            case "Friends":
+                setEmotions(FRIENDS_MAIN, bottomButtonsArray: FRIENDS_JOINTS)
+                break
+            case "Places":
+                setEmotions(PLACES_MAIN, bottomButtonsArray: PLACES_JOINTS)
+                break
+            case "School":
+                setEmotions(SCHOOL_MAIN, bottomButtonsArray: SCHOOL_JOINTS)
+                break
+            default:
+                break
+        }
+    }
+    
     
     func appendText(text:String) {
         textField.text = textField.text + text + " "
+    }
+    
+    func setEmotions(emotions:NSArray, bottomButtonsArray:NSArray){
+        setBottomButtonsTitles(bottomButtonsArray)
+        setMainButtons(emotions)
+    }
+    
+    func setBottomButtonsTitles(bottomButtonsArray:NSArray){
+        for (index, title) in enumerate(bottomButtonsArray){
+            bottomButtons[index].setTitle(title as? String, forState: UIControlState.Normal)
+        }
+    }
+    
+    func setMainButtons(emotions:NSArray){
+        for (index, title) in enumerate(emotions){
+            buttons[index].setTitle(title as? String, forState: UIControlState.Normal)
+        }
+    }
+    
+    func setCategoryButtonsTitles(categoryButtonTitles:NSArray){
+        for (index, title) in enumerate(categoryButtonTitles){
+            categoryButtons[index].setTitle(title as? String, forState: UIControlState.Normal)
+        }
     }
 }
 
